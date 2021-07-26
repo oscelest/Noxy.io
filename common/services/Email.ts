c7b8ce33621d60fb957666c0136487b390f5784d
oscelest <oscelest@gmail.com>
2021-07-26 17:29:02 +0200

    Moving services, enums and classes into a shared common library.

--- common/services/Email.ts
+++ common/services/Email.ts
@@ -1,29 +1,42 @@
 import {SendEmailCommand, SendEmailRequest, SESClient} from "@aws-sdk/client-ses";
 import Logger from "./Logger";
+import Level = Logger.Level;
 
-if (!process.env.MAIL_ID) throw new Error("MAIL_ID environmental value must be defined.");
-if (!process.env.MAIL_REGION) throw new Error("MAIL_REGION environmental value must be defined.");
-if (!process.env.MAIL_SECRET) throw new Error("MAIL_SECRET environmental value must be defined.");
+const default_region = "n/a";
+const default_access_key = "";
+const default_secret_key = "";
 
-const client = new SESClient({
-  region:      process.env.MAIL_REGION,
-  credentials: {
-    accessKeyId:     process.env.MAIL_ID!,
-    secretAccessKey: process.env.MAIL_SECRET!,
-  },
-});
+let is_stubbed = !process.env.MAIL_ID || !process.env.MAIL_REGION || !process.env.MAIL_SECRET;
+if (!process.env.MAIL_ID) Logger.write(Level.INFO, "MAIL_ID environmental value is not defined. Email will be stubbed.");
+if (!process.env.MAIL_REGION) Logger.write(Level.INFO, "MAIL_REGION environmental value is not defined. Email will be stubbed.");
+if (!process.env.MAIL_SECRET) Logger.write(Level.INFO, "MAIL_SECRET environmental value is not defined. Email will be stubbed.");
 
 module Email {
 
+  let region: string = !is_stubbed ? process.env.MAIL_REGION! : default_region;
+  let access_key: string = !is_stubbed ? process.env.MAIL_ID! : default_access_key;
+  let secret_key: string = !is_stubbed ? process.env.MAIL_SECRET! : default_secret_key;
+
+  export const client = new SESClient({
+    region:      region,
+    credentials: {
+      accessKeyId:     access_key,
+      secretAccessKey: secret_key,
+    },
+  });
+
   export async function send(request: SendEmailRequest) {
     try {
+      if (await client.config.region() === default_region) return console.log(request);
       await client.send(new SendEmailCommand(request));
     }
     catch (error) {
       Logger.write(Logger.Level.ERROR, error);
     }
   }
-
 }
+
+console.log(Email.client);
+console.log(Email.client.config.region());
 
 export default Email;
